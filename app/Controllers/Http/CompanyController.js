01903181974c7fa22aecba63c866a126afa88782
Luis Garcia <luisgarcia173@gmail.com>
2019-11-27 00:52:22 -0300

    Update services

--- app/Controllers/Http/CompanyController.js
+++ app/Controllers/Http/CompanyController.js
@@ -4,6 +4,7 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Company = use('App/Models/Company')
 /**
  * Resourceful controller for interacting with companies
  */
@@ -17,19 +18,9 @@ class CompanyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index({ request, response, view }) {
 
-  /**
-   * Render a form to be used for creating a new company.
-   * GET companies/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return await Company.all()
   }
 
   /**
@@ -40,7 +31,16 @@ class CompanyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    const data = request.only([
+      'username',
+      'password',
+      'email',
+      'photo',
+      'name'
+    ])
+
+    return await Company.create(data)
   }
 
   /**
@@ -52,19 +52,10 @@ class CompanyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing company.
-   * GET companies/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
+    
+    const company = await Company.findOrFail(params.id)
+    return company
   }
 
   /**
@@ -75,7 +66,20 @@ class CompanyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
+  
+    const company = await Company.findOrFail(params.id)
+    const data = request.only([
+      'username',
+      'password',
+      'email',
+      'photo',
+      'name'
+    ])
+
+    company.merge(data)
+    await company.save()
+    return company
   }
 
   /**
@@ -86,7 +90,10 @@ class CompanyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
+    
+    const company = await Company.findOrFail(params.id)
+    await company.delete()
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
+Route.group(() => {
+  Route.resource('company', 'CompanyController').apiOnly()
+  Route.resource('opportunity', 'OpportunityController').apiOnly()
 })
